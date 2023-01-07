fffa3a7dc699110cbded17991adafff2424201e8
DanielNptk <danylonepotiuk@gmail.com>
2023-01-07 23:57:33 +0100

    Adding state

--- src/components/NewsBlock.jsx
+++ src/components/NewsBlock.jsx
@@ -1,11 +1,22 @@
 import { Reaction } from "./Reaction"
+import { useState } from "react";
+import { ReactionsList } from "./ReactionsList";
 
 export const NewsBlock = (props) => {
+    const [showReactions, setShowReactions] = useState(false);
+
+    function switchReactions() {
+        setShowReactions(!showReactions);
+    }
+
     return (
-        <div className="news-block">
-            <div className="headline">{props.headline}</div>
-            <div className="news-content">{props.content}</div>
-            <Reaction />
-        </div>
+        <>
+            <div className="news-block" onClick={switchReactions}>
+                <div className="headline">{props.headline}</div>
+                <div className="news-content">{props.content}</div>
+                <Reaction />
+            </div>
+            {showReactions && <ReactionsList />}
+        </>
     )
 }
--- src/components/ReactionsList.jsx
+++ src/components/ReactionsList.jsx
@@ -1,3 +1,5 @@
 export const ReactionsList = () => {
-    
+    return (
+        <div style={{width: "50px", height: "50px", backgroundColor: "tomato"}}></div>
+    )
 }
